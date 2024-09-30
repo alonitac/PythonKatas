3b70c9d896a54c68542e68ab2b0ebb1df376bd62
alonit <alon.itach@mail.huji.ac.il>
2024-09-30 21:22:17 +0300

    initial commit

--- katas/test/test_binary_to_dec.py
+++ katas/test/test_binary_to_dec.py
@@ -1,15 +1,9 @@
 import unittest
-from python_katas.binary_to_dec import binary_to_dec
 
 
 class TestBinaryToDecL1(unittest.TestCase):
     def test_binary_to_dec_valid_input(self):
-        result = binary_to_dec("001011101111011000")
-        self.assertEqual(result, 48088)
-
-    def test_binary_to_dec_invalid_input(self):
-        with self.assertRaises(ArithmeticError):
-            binary_to_dec("0010154431101111012000")
+        pass
 
 
 if __name__ == '__main__':
