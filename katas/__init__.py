c12aa9e46e80e0e5c01470873a40518420a5728d
alonit <alon.itach@mail.huji.ac.il>
2024-09-30 21:24:54 +0300

    initial commit

--- katas/__init__.py
+++ katas/__init__.py
@@ -1,165 +1,165 @@
-katas = [
-    'hello_world.py',                   # function structure
-    'hello_world2.py',                  # str type, concat
-    'numbers_division.py',              # int and float types
-    'personalized_hello_world.py',      # function arguments
-    'age_message_fix.py',               # dynamically typed, can't concat str + int.
-    'get_seconds.py',                   # math operators
-    'get_century.py',                   # math operators
-    'bad_average.py',                   # parentheses in math
-    'calculator1.py',                   # built in functions type function call
-    'calculator2.py',                   # convert types calculator2 str + str
-    'tax_calc.py',                      # function with multiple arguments, variable scope
-    'bad_function.py',                  # function troubleshoot
-    'fill_a_cab.py',                    # simple if  # TODO explain syntax
-    'time_convertor.py',                # if, ==, elif
-    'is_even.py',                       # bool
-    'can_drive.py',                     # and
-    'can_drive2.py',                    # or operators
-    'assess_temperature.py',            # if, elif, else
-    'my_abs.py',                        # abs (abs without abs)
-    'shopping_list.py',                 # intro list
-    'last_one.py',                      # simple element choose in list
-    'sandwich_ingredients.py',          # simple slice
-    'every_second_word.py',             # slice with jumps
-    'recent_messages.py',               # slice to ends
-    'get_username.py',                  # slice of string
-    'shopping_list2.py',                # add to list
-    'log_message.py',                   # Print vs return (func return None)
-    '',                                 # default values arguments
-    'mailing_list.py',                  # two functions: concat strings and split
-    'grader.py',                        # multiple returns in function
-    'find_student.py',                  # in operator (list, string)
-    'is_word_absent.py',                # not in
-    'is_valid_password',                # len built in function
-    'is_earlier.py',                    # compare two lists
-    'num_of_digits.py',                 # total digits (int to str conversion)
-    'is_palindrome.py',                 # reverse string
-    'has_pattern.py',                   # slice in if
-    'is_subscriber_matching.py'         # Compare case-insensitive values (str.lower())
-    'get_first_n_words.py',             # str: split, join
-    'clean_text.py',                    # str: replace
-    'get_user_info.py'                  # tuple, return multiple values
-    'fraction_of_float.py'              # type convertion: float-int or float-str
-    'start_end.py'                      # slice, str, check user input
-    'verbing.py'                        # str endswith
-    'print_list_elements.py'            # for loop simple 
-    'total_expense.py'                  # for with aggregator variable
-    'min_max.py'                        # max, min implementation with for loops
-    'mailing_list2.py'                  # override element in a list
-    'is_prime.py'                       # built in: range
-    'list_diff.py'                      # for loop with range() index
-    'under_18.py'                       # counter outside loop   
-    'even_sublist.py'                   # for loop
-    'word_count.py'                     # word count 
-    'sum_even_numbers.py'               # fix error (range(lst) instead range(len(lst)))
-    'app_health.py'                     # global variable
-    'max_difference.py'                 # for in for
-    'name_pair.py'                      # all pairs in a list (for in for)
-    'validate_age.py'                   # user input validation, isinstance() 
-    'is_unique_str.py'                  # set: is unique str
-    'reformat_phone.py'                 # reformat phone number: isdigit()
-    'swap_in_list.py'                   # swap
-    'partial_list.py'                   # partial list 
-    'lottery_guess.py'                  # import random
-    'summarize_scores.py'               # function declaration
-    'all_positive.py'                   # all() built in
-    'long_str.py'                       # any() built in
-    'currency_convert.py'               # dict intro: convert currency
-    'get_department.py',                # get value by key
-    'course_enrollment.py'              # working with two dicts
-    'contacts.py'                       # keys assign, dict mutability 
-    'phone_lookup.py'                   # dict: in operator, get()
-    'fix_movie_review.py'               # check if key is in dict (fix common error)
-    'overweight.py'                     # dict.items()
-    'find_item_price.py'                # nested dict
-    'check_identity.py'                 # the `is` operator
-    'book_your_seat.py'                 # is None
-    'boarding_pass_code.py'             # f string 
-    'process_payment.py'                # type() or isinstance(): multiple input type for an argument
-    
-    'lets_vote.py'                      # increase counter in dict (add if not exist)
-    'in_the_club.py'                    # eligible enter the club (id or passport and above 18)
-    'top_student.py'                    # lambda in `max` function 
-    'mind_the_gap.py'                   # manually count spaces in string (str.count())
-    'is_sublist.py'                     # sublist check
-    'runner_position.py'                # find the index (list.index(), but do it manually)
-    'folder_count.py'                   # get filename by path check valid path
-    'secret_club.py'                    # dict from two lists
-    'alex_has_ocd.py'                   # list of tuples from dict
-    'they_legit.py'                     # list of dict, lambda functions
-    'do_twice.py'                       # lambda functions
-    'list_rotation.py'                  # list rotation
-    'count_even_numbers.py'             # return terminates the function (bug)
-    'time_me.py'                        # import time
-
-    ''  # toward binary search
-    ''  # add if not exists (list and dict) (set)
-
-]
-fix_me.py
-knapsack.py
-tasks_scheduling.py
-pascal_triangle.py
-youtube.py
-bash.py
-count_vowels.py
-bad_average.py
-to_lower_case.py
-complete_me.py
-sum_of_elements.py
-words_concat.py
-monotonic_array.py
-palindrome.py
-prime_num.py
-seven_boom.py
-strong_pass.py
-merge_sorted_lists.py
-best_student.py
-pair_match.py
-merge_dict.py
-summer.py
-files_backup.py
-most_frequent_name.py
-replace_in_file.py
-github_status.py
-reviewed_pull_requests.py
-file_exceptions.py
-binary_to_dec.py
-is_valid_email.py
-car.py
-dog.py
-cache_list.py
-simple_queue.py
-util_package.py
-custom_import.py
-import_warning.py
-start_end_v2.py
-sum_of_digits.py
-reverse_words_concat.py
-matrix_avg.py
-do_n_times.py
-ceaser_cipher.py
-is_unique_str.py
-merge_dict_v2.py
-name_histogram.py
-scrooge_customers.py
-sjf.py
-nginx_log_parser.py
-json_configs_merge.py
-requests_retry.py
-requests_timeout.py
-tree.py
-sorted_dict.py
-custom_exception.py
-messy_module.py
-list_flatten.py
-longest_common_prefix.py
-rotate_matrix.py
-longest_common_substring.py
-valid_parentheses.py
-valid_git_tree.py
-ansible_dynamic_inv.py
-queue_with_failover.py
-unittesting.py
-singleton.py
+# katas = [
+#     'hello_world.py',                   # function structure
+#     'hello_world2.py',                  # str type, concat
+#     'numbers_division.py',              # int and float types
+#     'personalized_hello_world.py',      # function arguments
+#     'age_message_fix.py',               # dynamically typed, can't concat str + int.
+#     'get_seconds.py',                   # math operators
+#     'get_century.py',                   # math operators
+#     'bad_average.py',                   # parentheses in math
+#     'calculator1.py',                   # built in functions type function call
+#     'calculator2.py',                   # convert types calculator2 str + str
+#     'tax_calc.py',                      # function with multiple arguments, variable scope
+#     'bad_function.py',                  # function troubleshoot
+#     'fill_a_cab.py',                    # simple if  # TODO explain syntax
+#     'time_convertor.py',                # if, ==, elif
+#     'is_even.py',                       # bool
+#     'can_drive.py',                     # and
+#     'can_drive2.py',                    # or operators
+#     'assess_temperature.py',            # if, elif, else
+#     'my_abs.py',                        # abs (abs without abs)
+#     'shopping_list.py',                 # intro list
+#     'last_one.py',                      # simple element choose in list
+#     'sandwich_ingredients.py',          # simple slice
+#     'every_second_word.py',             # slice with jumps
+#     'recent_messages.py',               # slice to ends
+#     'get_username.py',                  # slice of string
+#     'shopping_list2.py',                # add to list
+#     'log_message.py',                   # Print vs return (func return None)
+#     '',                                 # default values arguments
+#     'mailing_list.py',                  # two functions: concat strings and split
+#     'grader.py',                        # multiple returns in function
+#     'find_student.py',                  # in operator (list, string)
+#     'is_word_absent.py',                # not in
+#     'is_valid_password',                # len built in function
+#     'is_earlier.py',                    # compare two lists
+#     'num_of_digits.py',                 # total digits (int to str conversion)
+#     'is_palindrome.py',                 # reverse string
+#     'has_pattern.py',                   # slice in if
+#     'is_subscriber_matching.py'         # Compare case-insensitive values (str.lower())
+#     'get_first_n_words.py',             # str: split, join
+#     'clean_text.py',                    # str: replace
+#     'get_user_info.py'                  # tuple, return multiple values
+#     'fraction_of_float.py'              # type convertion: float-int or float-str
+#     'start_end.py'                      # slice, str, check user input
+#     'verbing.py'                        # str endswith
+#     'print_list_elements.py'            # for loop simple
+#     'total_expense.py'                  # for with aggregator variable
+#     'min_max.py'                        # max, min implementation with for loops
+#     'mailing_list2.py'                  # override element in a list
+#     'is_prime.py'                       # built in: range
+#     'list_diff.py'                      # for loop with range() index
+#     'under_18.py'                       # counter outside loop
+#     'even_sublist.py'                   # for loop
+#     'word_count.py'                     # word count
+#     'sum_even_numbers.py'               # fix error (range(lst) instead range(len(lst)))
+#     'app_health.py'                     # global variable
+#     'max_difference.py'                 # for in for
+#     'name_pair.py'                      # all pairs in a list (for in for)
+#     'validate_age.py'                   # user input validation, isinstance()
+#     'is_unique_str.py'                  # set: is unique str
+#     'reformat_phone.py'                 # reformat phone number: isdigit()
+#     'swap_in_list.py'                   # swap
+#     'partial_list.py'                   # partial list
+#     'lottery_guess.py'                  # import random
+#     'summarize_scores.py'               # function declaration
+#     'all_positive.py'                   # all() built in
+#     'long_str.py'                       # any() built in
+#     'currency_convert.py'               # dict intro: convert currency
+#     'get_department.py',                # get value by key
+#     'course_enrollment.py'              # working with two dicts
+#     'contacts.py'                       # keys assign, dict mutability
+#     'phone_lookup.py'                   # dict: in operator, get()
+#     'fix_movie_review.py'               # check if key is in dict (fix common error)
+#     'overweight.py'                     # dict.items()
+#     'find_item_price.py'                # nested dict
+#     'check_identity.py'                 # the `is` operator
+#     'book_your_seat.py'                 # is None
+#     'boarding_pass_code.py'             # f string
+#     'process_payment.py'                # type() or isinstance(): multiple input type for an argument
+#
+#     'lets_vote.py'                      # increase counter in dict (add if not exist)
+#     'in_the_club.py'                    # eligible enter the club (id or passport and above 18)
+#     'top_student.py'                    # lambda in `max` function
+#     'mind_the_gap.py'                   # manually count spaces in string (str.count())
+#     'is_sublist.py'                     # sublist check
+#     'runner_position.py'                # find the index (list.index(), but do it manually)
+#     'folder_count.py'                   # get filename by path check valid path
+#     'secret_club.py'                    # dict from two lists
+#     'alex_has_ocd.py'                   # list of tuples from dict
+#     'they_legit.py'                     # list of dict, lambda functions
+#     'do_twice.py'                       # lambda functions
+#     'list_rotation.py'                  # list rotation
+#     'count_even_numbers.py'             # return terminates the function (bug)
+#     'time_me.py'                        # import time
+#
+#     ''  # toward binary search
+#     ''  # add if not exists (list and dict) (set)
+#
+# ]
+# fix_me.py
+# knapsack.py
+# tasks_scheduling.py
+# pascal_triangle.py
+# youtube.py
+# bash.py
+# count_vowels.py
+# bad_average.py
+# to_lower_case.py
+# complete_me.py
+# sum_of_elements.py
+# words_concat.py
+# monotonic_array.py
+# palindrome.py
+# prime_num.py
+# seven_boom.py
+# strong_pass.py
+# merge_sorted_lists.py
+# best_student.py
+# pair_match.py
+# merge_dict.py
+# summer.py
+# files_backup.py
+# most_frequent_name.py
+# replace_in_file.py
+# github_status.py
+# reviewed_pull_requests.py
+# file_exceptions.py
+# binary_to_dec.py
+# is_valid_email.py
+# car.py
+# dog.py
+# cache_list.py
+# simple_queue.py
+# util_package.py
+# custom_import.py
+# import_warning.py
+# start_end_v2.py
+# sum_of_digits.py
+# reverse_words_concat.py
+# matrix_avg.py
+# do_n_times.py
+# ceaser_cipher.py
+# is_unique_str.py
+# merge_dict_v2.py
+# name_histogram.py
+# scrooge_customers.py
+# sjf.py
+# nginx_log_parser.py
+# json_configs_merge.py
+# requests_retry.py
+# requests_timeout.py
+# tree.py
+# sorted_dict.py
+# custom_exception.py
+# messy_module.py
+# list_flatten.py
+# longest_common_prefix.py
+# rotate_matrix.py
+# longest_common_substring.py
+# valid_parentheses.py
+# valid_git_tree.py
+# ansible_dynamic_inv.py
+# queue_with_failover.py
+# unittesting.py
+# singleton.py
